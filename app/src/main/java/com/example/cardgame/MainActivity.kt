93ada3a88e6f30efeefc3006780412c5eae5f801
ElDavido81 <eldavido81@gmail.com>
2024-11-20 09:54:35 +0100

    cards visible

--- app/src/main/java/com/example/cardgame/MainActivity.kt
+++ app/src/main/java/com/example/cardgame/MainActivity.kt
@@ -1,12 +1,18 @@
 package com.example.cardgame
 
 import android.os.Bundle
+import android.widget.ImageView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var leftcard : ImageView
+    lateinit var rightcard : ImageView
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -16,5 +22,13 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        leftcard = findViewById(R.id.iv_card1)
+        leftcard.setOnClickListener{
+            leftcard.drawable
+        }
+
+        rightcard = findViewById(R.id.iv_card2)
+
     }
 }
